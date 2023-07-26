6e20f69876c3aa2ef55b2f81d67277afc231895c
BottleBear-github <pbw1283@gmail.com>
2023-07-26 11:05:38 +0900

    1

--- src/test/java/com/pbw/main/bankBook/BankBookDAOTest.java
+++ src/test/java/com/pbw/main/bankBook/BankBookDAOTest.java
@@ -6,18 +6,32 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.pbw.main.Mytest;
+import com.pbw.main.bankbook.BankBookDAO;
 import com.pbw.main.bankbook.BankBookDTO;
 
 public class BankBookDAOTest extends Mytest {
 
-//	private SqlSession sqlSession;
-//	
-//	private final String NAMESPACE="com.pbw.main.bankbook.BankBookDAO.";
-//	
-//	public List<BankBookDTO> getList() throws Exception {
-//		return sqlSession.selectList(NAMESPACE+"getList");
-//	}
+	@Autowired
+	private BankBookDAO bankBookDAO;
+	
+	@Test
+	public void addTest() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		
+		for(int i=0; i<30; i++) {
+			
+			bankBookDTO.setBookName("청년병웅통장"+i);
+			bankBookDTO.setBookContents("입출금 내용 및 내역"+i);
+			bankBookDTO.setBookRate(8.7);
+			bankBookDTO.setBookSale(1);
+			bankBookDAO.setAdd(bankBookDTO);
+		}
+		
+		System.out.println("Finish");
+		
+	}
 
 }
